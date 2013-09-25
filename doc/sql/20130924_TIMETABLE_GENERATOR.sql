6d6342d56e7e951453b4d4b5d890eaad269a069c
Helio Albano <helioalb@gmail.com>
2013-09-24 22:19:25 -0300

    Added foreign key in sql

--- doc/sql/20130924_TIMETABLE_GENERATOR.sql
+++ doc/sql/20130924_TIMETABLE_GENERATOR.sql
@@ -1,9 +1,10 @@
 CREATE TABLE route
 (
-id          integer,
+id          serial,
+bus_id      integer,
 origin      varchar(30) NOT NULL,--eg: Avelino Vieira
 destination varchar(30) NOT NULL,--eg: Terminal Central
-CONSTRAINT pk_route_id PRIMARY KEY(id, origin, destination)
+CONSTRAINT pk_route_id PRIMARY KEY(id)
 );
 
 CREATE TABLE bus_stop
@@ -17,9 +18,11 @@ CONSTRAINT pk_bus_stop_id PRIMARY KEY(id)
 
 CREATE TABLE route_has_bus_stop
 (
-bus_id        integer,
+route_id        integer,
 bus_stop_id   integer,
 moment        time,
 
-CONSTRAINT pk_route_has_bus_stop PRIMARY KEY(bus_id, bus_stop_id, moment)
+CONSTRAINT pk_route_has_bus_stop PRIMARY KEY(route_id, bus_stop_id, moment),
+CONSTRAINT fk_route FOREIGN KEY (route_id) REFERENCES route(id),
+CONSTRAINT fk_bus_stop FOREIGN KEY (bus_stop_id) REFERENCES bus_stop(id)
 );
