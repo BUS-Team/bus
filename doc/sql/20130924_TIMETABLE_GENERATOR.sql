fdf14516e3f9fe83fcdcb999e1d1b17ae2f57cde
Vinícius Fernandes de Jesus <viniciusffj@gmail.com>
2013-09-25 10:47:11 -0300

    Added the column 'name' in table bus_stop.

--- doc/sql/20130924_TIMETABLE_GENERATOR.sql
+++ doc/sql/20130924_TIMETABLE_GENERATOR.sql
@@ -1,28 +1,29 @@
 CREATE TABLE route
 (
-id          serial,
-bus_id      integer,
-origin      varchar(30) NOT NULL,--eg: Avelino Vieira
-destination varchar(30) NOT NULL,--eg: Terminal Central
-CONSTRAINT pk_route_id PRIMARY KEY(id)
+	id          serial,
+	bus_id      integer,
+	origin      varchar(30) NOT NULL,--eg: Avelino Vieira
+	destination varchar(30) NOT NULL,--eg: Terminal Central
+	CONSTRAINT pk_route_id PRIMARY KEY(id)
 );
 
 CREATE TABLE bus_stop
 (
-id        integer,
-latitude  float NOT NULL,
-longitude float NOT NULL,
+	id        integer,
+	latitude  float NOT NULL,
+	longitude float NOT NULL,
+	name 	  varchar(30) NOT NULL,
 
-CONSTRAINT pk_bus_stop_id PRIMARY KEY(id)
+	CONSTRAINT pk_bus_stop_id PRIMARY KEY(id)
 );
 
 CREATE TABLE route_has_bus_stop
 (
-route_id        integer,
-bus_stop_id   integer,
-moment        time,
+	route_id      integer,
+	bus_stop_id   integer,
+	moment        time,
 
-CONSTRAINT pk_route_has_bus_stop PRIMARY KEY(route_id, bus_stop_id, moment),
-CONSTRAINT fk_route FOREIGN KEY (route_id) REFERENCES route(id),
-CONSTRAINT fk_bus_stop FOREIGN KEY (bus_stop_id) REFERENCES bus_stop(id)
+	CONSTRAINT pk_route_has_bus_stop PRIMARY KEY(route_id, bus_stop_id, moment),
+	CONSTRAINT fk_route FOREIGN KEY (route_id) REFERENCES route(id),
+	CONSTRAINT fk_bus_stop FOREIGN KEY (bus_stop_id) REFERENCES bus_stop(id)
 );
