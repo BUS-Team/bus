d28fe58384137e734bac2d9c3022ef3cfc37e0ea
gjhenrique <gjhenrique@gmail.com>
2013-12-02 05:21:21 -0200

    Added structure generated by architetype

---
+++ android/bus-android-it/src/main/java/br/com/bus/android/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package br.com.bus.android.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import br.com.bus.android.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
