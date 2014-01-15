9ac807c74b364848fd04c14a67f58e6b7798805f
Breno Naodi Kusunoki <brenokusunoki@gmail.com>
2014-01-14 23:20:55 -0200

    Moves database configuration to a config file.

--- webservice/src/main/java/team/bus/model/dao/implementation/JDBCDAO.java
+++ webservice/src/main/java/team/bus/model/dao/implementation/JDBCDAO.java
@@ -1,24 +1,47 @@
 package team.bus.model.dao.implementation;
 
 import java.sql.Connection;
+
+import java.util.Properties;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import team.bus.model.bean.DataBaseInfo;
 
 public abstract class JDBCDAO {
 
+    private static String host;
+    //private static String port;
+    private static String user;
+    private static String password;
+    private static String dbName;
     protected DataBaseInfo dataBaseInfo = null;
 
     public JDBCDAO() {
-        //TODO: [HA][URGENT] Retirar essa definição daqui.
-        DataBaseInfo dbi = new DataBaseInfo()
-                .withHost("localhost")
-                .withDataBase("bus")
-                .withUser("postgres")
-                .withPassword("123456");
+        Properties config = new Properties();
+        try {
+            //TODO: [HA][URGENT] Test this configuration.
+            config.load(JDBCDAO.class.getClassLoader().getResourceAsStream("/database_config.properties"));
 
-        this.dataBaseInfo = dbi;
+            host     = config.getProperty("host");
+            //port     = config.getProperty("port");
+            user     = config.getProperty("user");
+            password = config.getProperty("password");
+            dbName   = config.getProperty("dbName");
+
+            //TODO: [HA][URGENT] Retirar essa definição daqui.
+            DataBaseInfo dbi = new DataBaseInfo()
+                    .withHost(host)
+                    .withDataBase(dbName)
+                    .withUser(user)
+                    .withPassword(password);
+            this.dataBaseInfo = dbi;
+        } catch (Exception e) {
+            Logger.getLogger(JDBCDAO.class.getName()).log(Level.CONFIG, null, e);
+        }
     }
 
     protected Connection createConnection() throws SQLException,
