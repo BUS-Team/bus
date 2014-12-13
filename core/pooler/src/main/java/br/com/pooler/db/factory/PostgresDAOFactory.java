88ef4e11601d016ed5ce0be4eb3e9d74adc8a828
Breno Kusunoki <brenokusunoki@gmail.com>
2014-12-13 19:01:49 -0200

    #36 - Added configuration using Yaml

--- core/pooler/src/main/java/br/com/pooler/db/factory/PostgresDAOFactory.java
+++ core/pooler/src/main/java/br/com/pooler/db/factory/PostgresDAOFactory.java
@@ -1,23 +1,41 @@
 package br.com.pooler.db.factory;
 
+import java.io.*;
 import java.sql.*;
+import java.util.Map;
 
 import br.com.pooler.db.dao.DAOResponsePosition;
 import br.com.pooler.db.dao.PostgresDAOResponsePosition;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * Created by breno on 13/12/14.
  */
 public class PostgresDAOFactory extends DAOFactory {
+    InputStream input;
+    Map<String,String> conf;
 
     private static Connection connection = null;
-    private String driverName = "org.postgresql.Driver";
-    private String url = "jdbc:postgresql://127.0.0.1:5432/tcglpooling";
-    private String user = "breno";
-    private String password = "root";
+    private String driverName;
+    private String url;
+    private String user;
+    private String password;
 
     public PostgresDAOFactory() throws ClassNotFoundException {
-        Class.forName(driverName);
+        try {
+            input = this.getClass().getResourceAsStream("/config.yml");
+            conf = (Map<String, String>) new Yaml().load(input);
+
+            driverName = conf.get("driverName");
+            url = conf.get("url");
+            user = conf.get("user");
+            password = conf.get("password");
+
+            Class.forName(driverName);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
